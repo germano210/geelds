b16058a2dd971e9f305c08717ddf24cd746c599b
germano210 <2024020049@aluno.osorio.ifrs.edu.br>
2026-05-05 20:14:30 -0300

    refactor(test) remove builds from initialize.

--- pokedex-api/src/main/java/br/com/pokemon/pokedex_api/PokedexApiApplication.java
+++ pokedex-api/src/main/java/br/com/pokemon/pokedex_api/PokedexApiApplication.java
@@ -22,52 +22,52 @@ public class PokedexApiApplication {
         SpringApplication.run(PokedexApiApplication.class, args);
     }
 
-    @Bean
-    public CommandLineRunner testarLeitorSmogon(
-            PokemonBuildService buildService,
-            PasteService smogonParser,
-            PokeApiService pokeApiService) { // <-- INJETAMOS O NOVO SERVIÇO AQUI
-
-        return args -> {
-            System.out.println("=== INICIANDO TESTE DO LEITOR SMOGON ===");
-
-            String smogonText = "Cloyster @ White Herb\n" +
-                    "Ability: Skill Link\n" +
-                    "EVs: 252 Atk / 4 SpA / 252 Spe\n" +
-                    "Jolly Nature\n" +
-                    "- Shell Smash\n" +
-                    "- Icicle Spear\n" +
-                    "- Rock Blast\n" +
-                    "- Hydro Pump" ;
-
-            try {
-                PasteDTO resultado = smogonParser.parseSmogonText(smogonText);
-                System.out.println("Pokémon lido: " + resultado.getPokemonName());
-
-                // MÁGICA ACONTECENDO: Buscamos o ID dinamicamente na PokeAPI!
-                Integer pokemonId = pokeApiService.getPokemonIdByName(resultado.getPokemonName());
-
-                if (pokemonId == null) {
-                    System.out.println("Pokémon não encontrado na PokeAPI. Cancelando salvamento.");
-                    return; // Interrompe o teste se o nome estiver errado
-                }
-
-                System.out.println("ID descoberto na PokeAPI: " + pokemonId);
-
-                PokemonBuild buildParaSalvar = resultado.getBuild();
-                buildParaSalvar.setPokemonId(pokemonId); // Usamos o ID dinâmico
-                buildParaSalvar.setGuideText("Build importada automaticamente!");
-
-                PokemonBuild buildSalva = buildService.criarBuild(buildParaSalvar, resultado.getMoveNames());
-                System.out.println("SUCESSO! Build salva no banco com ID: " + buildSalva.getId());
-
-            } catch (Exception e) {
-                System.out.println("ERRO NO TESTE: " + e.getMessage());
-                e.printStackTrace();
-            }
-
-            System.out.println("=== FIM DO TESTE ===");
-        };
-    }
+//    @Bean
+//    public CommandLineRunner testarLeitorSmogon(
+//            PokemonBuildService buildService,
+//            PasteService smogonParser,
+//            PokeApiService pokeApiService) {
+//
+//        return args -> {
+//            System.out.println("=== INICIANDO TESTE DO LEITOR SMOGON ===");
+//
+//            String smogonText = "Cloyster @ White Herb\n" +
+//                    "Ability: Skill Link\n" +
+//                    "EVs: 252 Atk / 4 SpA / 252 Spe\n" +
+//                    "Jolly Nature\n" +
+//                    "- Shell Smash\n" +
+//                    "- Icicle Spear\n" +
+//                    "- Rock Blast\n" +
+//                    "- Hydro Pump" ;
+//
+//            try {
+//                PasteDTO resultado = smogonParser.parseSmogonText(smogonText);
+//                System.out.println("Pokémon lido: " + resultado.getPokemonName());
+//
+//                // MÁGICA ACONTECENDO: Buscamos o ID dinamicamente na PokeAPI!
+//                Integer pokemonId = pokeApiService.getPokemonIdByName(resultado.getPokemonName());
+//
+//                if (pokemonId == null) {
+//                    System.out.println("Pokémon não encontrado na PokeAPI. Cancelando salvamento.");
+//                    return; // Interrompe o teste se o nome estiver errado
+//                }
+//
+//                System.out.println("ID descoberto na PokeAPI: " + pokemonId);
+//
+//                PokemonBuild buildParaSalvar = resultado.getBuild();
+//                buildParaSalvar.setPokemonId(pokemonId); // Usamos o ID dinâmico
+//                buildParaSalvar.setGuideText("Build importada automaticamente!");
+//
+//                PokemonBuild buildSalva = buildService.criarBuild(buildParaSalvar, resultado.getMoveNames());
+//                System.out.println("SUCESSO! Build salva no banco com ID: " + buildSalva.getId());
+//
+//            } catch (Exception e) {
+//                System.out.println("ERRO NO TESTE: " + e.getMessage());
+//                e.printStackTrace();
+//            }
+//
+//            System.out.println("=== FIM DO TESTE ===");
+//        };
+//    }
 
 }
